b9cecb6d4e69bf24b033f224743f808674afef80
maratz <maratz375@gmail.com>
2022-03-30 13:17:44 +0500

    Алгоритмы и структуры данных на Python. Базовый курс Урок 3. Массивы. Кортежи. Множества. Списки. 6. В одномерном массиве найти сумму элементов, находящихся между минимальным и максимальным элементами.  Сами минимальный и максимальный элементы в сумму не включать. +

---
+++ algorithms_python_dz_03/algorithms_py_Zakiev_dz_03_06.py
@@ -0,0 +1,39 @@
+# Урок 3. Массивы. Кортежи. Множества. Списки.
+# 6. В одномерном массиве найти сумму элементов, находящихся между минимальным и максимальным элементами.
+# Сами минимальный и максимальный элементы в сумму не включать.
+
+import random
+
+
+def random_array(num_elements, min_range, max_range):
+    random_list = []
+    for i in range(num_elements):
+        # randint
+        random_list.append(random.randint(min_range, max_range))
+    return random_list
+
+
+def sum_elements(input_list):
+    m_set = set(input_list)
+    min_num = min(m_set)
+    max_num = max(m_set)
+    while input_list.count(min_num) != 0:
+        input_list.remove(min_num)
+    while input_list.count(max_num) != 0:
+        input_list.remove(max_num)
+    return min_num, max_num, sum(input_list)
+
+
+def main():
+    m_array = (random_array(20, 0, 1000))
+    print('В одномерном массиве найти сумму элементов, находящихся между минимальным и максимальным элементами. '
+          'Сами минимальный и максимальный элементы в сумму не включать.')
+    print('Исходный массив:')
+    print(m_array)
+    print(f'Между минимальным значением ({(sum_elements(m_array))[0]}) и максимальным значением'
+          f' ({(sum_elements(m_array))[1]}) сумма элементов равна: {(sum_elements(m_array))[2]}')
+
+
+if __name__ == '__main__':
+    main()
+#
